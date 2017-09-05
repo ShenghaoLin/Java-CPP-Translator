8aa47ae6e5ee9488d2d1334a9d60b8a389184095
wies <wies@cs.nyu.edu>
2017-09-05 11:55:34 -0400

    first commit

---
+++ output/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "java_lang.h"
+
+using namespace java::lang;
+using namespace std;
+
+int main(void) {
+  cout << "CompilationUnit" << endl;
+  cout << "PackageDeclaration" << endl;
+  cout << "QualifiedIdentifier" << endl;
+  cout << "ClassDeclaration" << endl;
+  cout << "Modifiers" << endl;
+  cout << "Modifier" << endl;
+  cout << "ClassBody" << endl;
+  cout << "MethodDeclaration" << endl;
+  cout << "Modifiers" << endl;
+  cout << "Modifier" << endl;
+  cout << "Modifier" << endl;
+  cout << "VoidType" << endl;
+  cout << "FormalParameters" << endl;
+  cout << "FormalParameter" << endl;
+  cout << "Modifiers" << endl;
+  cout << "Type" << endl;
+  cout << "QualifiedIdentifier" << endl;
+  cout << "Dimensions" << endl;
+  cout << "Block" << endl;
+  cout << "ExpressionStatement" << endl;
+  cout << "CallExpression" << endl;
+  cout << "SelectionExpression" << endl;
+  cout << "PrimaryIdentifier" << endl;
+  cout << "Arguments" << endl;
+  cout << "StringLiteral" << endl;
+  return 0;
+}
