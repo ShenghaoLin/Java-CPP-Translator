2f8c199d4b4a1a7ed422956a3f8825eca7be92e4
goki <goki@172-16-49-141.DYNAPOOL.NYU.EDU>
2017-10-28 16:14:44 -0400

    added hidden field to Field to determine inheritance when necessary

--- src/main/java/edu/nyu/oop/Field.java
+++ src/main/java/edu/nyu/oop/Field.java
@@ -7,6 +7,7 @@ public class Field {
 	public String field_type;
 	public String field_name;
 	public String initial;
+	public String inherited_from;
 
 	public Field(String access_modifier, boolean is_static, String field_type, String field_name, String initial) {
 		this.access_modifier = access_modifier;
@@ -14,5 +15,6 @@ public class Field {
 		this.field_type = field_type;
 		this.field_name = field_name;
 		this.initial = initial;
+		this.inherited_from = "";
 	}
 }
