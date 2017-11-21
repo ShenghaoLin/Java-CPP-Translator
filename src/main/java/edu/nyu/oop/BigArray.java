9a7c877a5490ccdba222b12e4c5936aed68c8eff
goki <goki@172-17-87-71.DYNAPOOL.NYU.EDU>
2017-11-21 18:08:58 -0500

    array stuff added

---
+++ src/main/java/edu/nyu/oop/BigArray.java
@@ -0,0 +1,33 @@
+package edu.nyu.oop;
+
+import java.lang.StringBuilder;
+
+public class BigArray {
+
+	public String className;
+	public String packageName;
+
+	public Array(String className, String packageName) {
+		this.className = className;
+		this.packageName = packageName.replace(".", "::") + "::" + className;
+	}
+	
+	public String dump() {
+		String startDeclaration = "template<>\njava::lang::Class __Array<"
+		StringBuilder temp = new StringBuilder(startDeclaration);
+		temp.append(packageName);
+		temp.append("::__class()\n")
+		temp.append("{\n");
+		temp.append("  static java::lang::Class k =\n");
+		temp.append("\tnew java::lang::__Class(literal(\"[L");
+		temp.append(packageName.replace("::", "."));
+		temp.append(";\"),\n");
+		temp.append("\t\t\t\t\t\t\tjava::lang::__Object::__class(),\n");
+		temp.append("\t\t\t\t\t\t\t");
+		temp.append(packageName.replace(className, "__" + className));
+		temp.append("::__class());\n");
+		temp.append("return k;\n");
+		temp.append("}\n");
+		return temp.toString();
+	}
+}
