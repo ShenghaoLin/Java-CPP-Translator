7034160c4f85e34e5798c567142e665b9828b263
goki <goki@172-17-86-126.DYNAPOOL.NYU.EDU>
2017-12-01 15:07:42 -0500

    smthn

--- output/main.cpp
+++ output/main.cpp
@@ -5,14 +5,13 @@
 using namespace java::lang;
 
 using namespace std;
-using namespace inputs::test005;
+using namespace inputs::test040;
 
 int32_t main (ArrayOfString args ){
-B b = __B::__init (new __B() );
-A a1 = __A::__init (new __A() );
-A a2 = b ;
-cout << a1 -> __vptr -> toString (a1 )-> data << endl ;
-cout << a2 -> __vptr -> toString (a2 )-> data << endl ;
+C c = __C::__init (new __C() );
+c -> __vptr -> m (c , __A::__init (new __A() ), (Object ) c );
+c -> __vptr -> m (c , c , __Object::__init (new __Object() ));
+c -> __vptr -> m (c , (Object ) c , c );
 return 0 ;
 }
 
