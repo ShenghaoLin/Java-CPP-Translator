fa64e37a68d15bdb164aac5186e3913ff4f30333
goki <goki@gokis-Air.fios-router.home>
2017-12-16 23:06:55 -0500

    Print runtime nodes

--- src/main/java/edu/nyu/oop/Phase5.java
+++ src/main/java/edu/nyu/oop/Phase5.java
@@ -76,9 +76,11 @@ public class Phase5 extends Visitor {
     public void print(GNode ast) {
         dispatch(ast);
         printer.flush();
-        // if (null != ast.getProperty("RuntimeInfo")) {
-        //     printer.p(ast.getProperty("RuntimeInfo").toString()).flush();
-        // }
+        if (null != ast.getProperty("RuntimeInfo")) {
+            printer.pln("namespace rt {");
+            printer.pln(ast.getProperty("RuntimeInfo").toString());
+            printer.pln("}").flush();
+        }
     }
 
     /* The claim placed in the beginning of cpp files */
